5903c3dfadc35b826d3e0df571cc8f99d47738c0
Igor Muniz <igormuniz@DESKTOP-GOMLF1G.localdomain>
2022-09-08 00:37:14 -0300

    Inserindo em DB

--- desafio2/node/index.js
+++ desafio2/node/index.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const mysql = require('mysql')
-const app = express()
-const port = 3000
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3000;
 const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database: 'nodedb'
-}
-// const connection = mysql.createConnection(config)
-
-// const sqlInsert = `INSERT INTO people(name) values ('Igor')`
-// connection.query(sqlInsert)
-// connection.end()    
-
-// const sqlSelect = async() => {
-//     try{
-//         const query = `SELECT name FROM people`
-//         const selectResult = await connection.query(query)
-//         return selectResult 
-//     }
-//     catch (err) {
-//         throw err;
-//     }
-// }
+    database: 'nodedb',
+    multipleStatements: true
+}   
 
 app.get('/', (req,res) =>{
-    res.send('<h1>Olá Mundo! Rodando meu primeiro app em Node.js</h1>')
+    const connection = mysql.createConnection(config);
+    connection.connect((err) => {
+        if (err) {
+          console.log("Database Connection Failed !!!", err);
+        } else {
+            console.log("Connected!")
+        const query = `INSERT INTO people (name) values ('Jamila'); SELECT name FROM people;`;
+        connection.query(query, function (err, result){
+            if (err) throw err;
+            console.log(result);
+            res.send(`<h1>Olá Mundo! Rodando meu primeiro app em Node.js</h1> ${result}`);
+        }).end()
+        }
+    });
 })
 
 app.listen(port, () => {
-    console.log('Rodando na porta ' + port)
+    console.log('Rodando na porta ' + port);
 })
